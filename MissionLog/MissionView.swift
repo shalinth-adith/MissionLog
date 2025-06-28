0c2aa59fdccb2d281e46dc1056dba05a920500cd
shalinth-adith <shalinth.adith@outlook.com>
2025-06-28 10:54:57 +0530

    created a mission view

---
+++ MissionLog/MissionView.swift
@@ -0,0 +1,18 @@
+//
+//  MissionView.swift
+//  MissionLog
+//
+//  Created by shalinth adithyan on 28/06/25.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MissionView()
+}
