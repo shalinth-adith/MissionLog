432e7942303c35bb3f9b8adc9325714bcfd1e31a
shalinth-adith <shalinth.adith@outlook.com>
2025-06-28 10:55:13 +0530

    updated mission view

--- MissionLog/MissionView.swift
+++ MissionLog/MissionView.swift
@@ -8,11 +8,43 @@
 import SwiftUI
 
 struct MissionView: View {
+    let mission : Mission
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            ScrollView{
+                VStack{
+                    Image(mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .containerRelativeFrame(.horizontal) { width, axis in
+                            width * 0.8
+                        }
+                    VStack(alignment: .leading){
+                        Text("Mission Highlights")
+                            .font(.title.bold())
+                            .padding(.bottom, 5)
+                        Text(mission.description)
+                    }
+                    .padding(.horizontal)
+                }
+                .padding(.bottom)
+            }
+            .navigationTitle(mission.displayName)
+            .navigationBarTitleDisplayMode(.inline)
+            .background(
+                LinearGradient(
+                    gradient: Gradient(colors: [Color.black, Color(red: 212/255, green: 175/255, blue: 55/255)]),
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+                )
+        }
     }
 }
 
 #Preview {
-    MissionView()
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    return MissionView(mission: missions[0])
+        .preferredColorScheme(.dark)
 }
