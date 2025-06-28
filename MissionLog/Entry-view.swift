ad6198e8fe2e5a5238b1dd849b3d60ae62f73df8
shalinth-adith <shalinth.adith@outlook.com>
2025-06-28 09:39:41 +0530

    entry screen

---
+++ MissionLog/Entry-view.swift
@@ -0,0 +1,18 @@
+//
+//  Entry-view.swift
+//  MissionLog
+//
+//  Created by shalinth adithyan on 28/06/25.
+//
+
+import SwiftUI
+
+struct Entry_view: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Entry_view()
+}
